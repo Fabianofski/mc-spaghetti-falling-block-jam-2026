fca7916524d39c0adfb493be30a8e824b5b1b7b1
Santum <39064333+santumerino@users.noreply.github.com>
2026-09-19 16:44:29 -0300

    add: Some days

--- ui/order_viewer/meal_viewer/meal_viewer.gd
+++ ui/order_viewer/meal_viewer/meal_viewer.gd
@@ -6,14 +6,14 @@ var meal: Meal
 @onready var ingredient_label: Label = $IngredientLabel
 
 func format_ingredients(accum: String, i: Ingredient) -> String:
-    return accum + i.name + "\n"
+	return accum + i.name + "\n"
 
 func set_meal(order_id: int, _meal: Meal): 
-    meal = _meal
-    meal_label.text = "#%d %s" % [order_id, meal.name]
-    ingredient_label.text = meal.ingredients.reduce(format_ingredients, "")
-    
-    meal.completed.connect(on_meal_complete)
-    
+	meal = _meal
+	meal_label.text = "#%d %s" % [order_id, meal.name]
+	ingredient_label.text = meal.ingredients.reduce(format_ingredients, "")
+	
+	meal.completed.connect(on_meal_complete)
+	
 func on_meal_complete():
-    meal_label.text = meal_label.text + " (Done)"
+	meal_label.text = meal_label.text + " (Done)"
