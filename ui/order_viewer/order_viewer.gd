fd5790524f13cc11a1713028564dccafdba0043e
Santum <39064333+santumerino@users.noreply.github.com>
2026-09-19 14:03:35 -0300

    add: Some sprites

--- ui/order_viewer/order_viewer.gd
+++ ui/order_viewer/order_viewer.gd
@@ -5,19 +5,25 @@ var order: Order
 @onready var meals: VBoxContainer = $Meals
 @onready var meal_viewer: PackedScene = preload("res://ui/order_viewer/meal_viewer/meal_viewer.tscn")
 @onready var time_left : ProgressBar = $TimeLeft
-    
+@onready var time_left_percentage: Label = $TimeLeft/Label
+
 func on_order_completed(): 
-    queue_free()
+	queue_free()
 
 func set_order(_order: Order):
-    order = _order
-    order.completed.connect(on_order_completed)
+	order = _order
+	order.completed.connect(on_order_completed)
 
-    for idx in len(order.meals): 
-        var meal = order.meals[idx]
-        var mv = meal_viewer.instantiate()
-        meals.add_child(mv)
-        mv.set_meal(idx + 1, meal)
-        
+	for idx in len(order.meals): 
+		var meal = order.meals[idx]
+		var mv = meal_viewer.instantiate()
+		meals.add_child(mv)
+		mv.set_meal(idx + 1, meal)
+		
 func _process(_delta: float) -> void:
-    time_left.value = order.timer / order.time 
+	time_left.value = order.timer / order.time
+	
+	if time_left.value > 0: time_left_percentage.text = str(int(time_left.value * 100)) + "%"
+	else: time_left_percentage.text = "MISSED"
+	
+	if time_left.value < 0.25 and not time_left.indeterminate: time_left.indeterminate = true
