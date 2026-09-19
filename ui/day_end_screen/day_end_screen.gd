56d0d012210126bc49a3667909f7e4847499cdcf
Santum <39064333+santumerino@users.noreply.github.com>
2026-09-19 20:51:53 -0300

    add: Some more things to win and lose screens (placeholder)

--- ui/day_end_screen/day_end_screen.gd
+++ ui/day_end_screen/day_end_screen.gd
@@ -1,7 +1,10 @@
 extends Control
 
+@onready var button: JuicyButtonAwwYiss = $Button
+
 func _ready() -> void:
 	SignalBus.day_finished.connect(enable_screen)
+	button.should_oscillate = true
 	
 func enable_screen():
 	visible = true
