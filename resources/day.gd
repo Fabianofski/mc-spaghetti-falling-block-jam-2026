154b33539d45c6c28c2e794e76f2f3287e6d3107
Santum <39064333+santumerino@users.noreply.github.com>
2026-09-20 14:07:17 -0300

    add: lil bg to title

--- resources/day.gd
+++ resources/day.gd
@@ -22,58 +22,58 @@ signal succeeded()
 signal failed()
 
 func reset(): 
-    score = 0
-    max_score = 0
-    state = DayState.Open
-    
+	score = 0
+	max_score = 0
+	state = DayState.Open
+	
 func success():
-    state = DayState.Success
-    state_changed.emit(state)
-    succeeded.emit()
-    completed.emit()
-    
+	state = DayState.Success
+	state_changed.emit(state)
+	succeeded.emit()
+	completed.emit()
+	
 func fail():
-    state = DayState.Failed
-    state_changed.emit(state)
-    failed.emit()
-    completed.emit()
-    
+	state = DayState.Failed
+	state_changed.emit(state)
+	failed.emit()
+	completed.emit()
+	
 func start():
-    state = DayState.InProgress
-    state_changed.emit(state)
+	state = DayState.InProgress
+	state_changed.emit(state)
 
 func add_score(add: int):
-    score += add
-    score_changed.emit(score)
-    
+	score += add
+	score_changed.emit(score)
+	
 func add_max_score(add: int):
-    max_score += add
+	max_score += add
 
 func get_grade_from_percentage(percentage: float) -> String:
-    var grades = [
-        {"threshold": 0.97, "grade": "A+"},
-        {"threshold": 0.93, "grade": "A"},
-        {"threshold": 0.90, "grade": "A-"},
-        {"threshold": 0.87, "grade": "B+"},
-        {"threshold": 0.83, "grade": "B"},
-        {"threshold": 0.80, "grade": "B-"},
-        {"threshold": 0.77, "grade": "C+"},
-        {"threshold": 0.73, "grade": "C"},
-        {"threshold": 0.70, "grade": "C-"},
-        {"threshold": 0.67, "grade": "D+"},
-        {"threshold": 0.63, "grade": "D"},
-        {"threshold": 0.60, "grade": "D-"},
-    ]
-    
-    for grade_entry in grades:
-        if percentage >= grade_entry.threshold:
-            return grade_entry.grade
-    
-    return "F"
+	var grades = [
+		{"threshold": 0.97, "grade": "A+"},
+		{"threshold": 0.93, "grade": "A"},
+		{"threshold": 0.90, "grade": "A-"},
+		{"threshold": 0.87, "grade": "B+"},
+		{"threshold": 0.83, "grade": "B"},
+		{"threshold": 0.80, "grade": "B-"},
+		{"threshold": 0.77, "grade": "C+"},
+		{"threshold": 0.73, "grade": "C"},
+		{"threshold": 0.70, "grade": "C-"},
+		{"threshold": 0.67, "grade": "D+"},
+		{"threshold": 0.63, "grade": "D"},
+		{"threshold": 0.60, "grade": "D-"},
+	]
+	
+	for grade_entry in grades:
+		if percentage >= grade_entry.threshold:
+			return grade_entry.grade
+	
+	return "F"
 
 func get_grade() -> String: 
-    if state == DayState.Failed: return "F"
-    var percentage = score / float(max_score)
-    var grade = get_grade_from_percentage(percentage)
-    return grade
-    
+	if state == DayState.Failed: return "F"
+	var percentage = score / float(max_score)
+	var grade = get_grade_from_percentage(percentage)
+	return grade
+	
--- scenes/main_menu.gd
+++ scenes/main_menu.gd
@@ -1,12 +1,19 @@
 extends Control
 
 @onready var credits: ColorRect = $Credits
+@onready var clouds: Sprite2D = $Clouds
 
 func _ready() -> void:
 	$"HBoxContainer/Start Button".should_oscillate = true
 	$"HBoxContainer/Credits Button".should_oscillate = true
 	$"HBoxContainer/Quit Button".should_oscillate = true
 	credits.hide()
+
+func _process(delta: float) -> void:
+	if clouds.global_position.x < 2690: clouds.global_position.x += delta * 10
+	elif clouds.global_position.x >= 2690:
+		clouds.global_position.x = -1280
+		clouds.global_position.y = randi_range(568, 768)
 
 func _on_start_button_button_up() -> void:
 	GameManager.reset()
