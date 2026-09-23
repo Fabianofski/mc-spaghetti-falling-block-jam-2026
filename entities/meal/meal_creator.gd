f6540d4bd76dbd5e831b74ce0b6386cb60a11eac
Fabian Friedrich <friedrich.fabian@gmx.net>
2026-09-23 21:21:00 +0200

    add: holding of pull thingy

--- entities/meal/meal_creator.gd
+++ entities/meal/meal_creator.gd
@@ -6,60 +6,83 @@ var cooking: bool = false
 @onready var timer: Timer = $CookTimer
 @onready var progress_bar: ProgressBar = $ProgressBar
 
-@onready var pull_thingy: Sprite2D = $PullThingy
+@onready var pull_thingy: PullThingy = $PullThingy
 var _pull_tween: Tween = null
 
+var cancel_time: float = 0
+var wait_time: float = 1
+
 func _ready() -> void:
-	body_entered.connect(on_ingredient_entered)
-	body_exited.connect(on_ingredient_exited)
-	
-	timer.timeout.connect(finish_meal)
-	
-func _process(_delta: float) -> void:
-	progress_bar.value = 1 - timer.time_left / timer.wait_time
-	
+    body_entered.connect(on_ingredient_entered)
+    body_exited.connect(on_ingredient_exited)
+    
+    wait_time = timer.wait_time
+    timer.timeout.connect(finish_meal)
+    
+    pull_thingy.cancelled.connect(cancel_meal)
+    pull_thingy.pressed.connect(create_meal)
+    
+func _process(delta: float) -> void:
+    if cooking:
+        progress_bar.value = 1 - timer.time_left / wait_time
+    else: 
+        progress_bar.value = 1 - cancel_time / wait_time
+        if cancel_time <= wait_time:
+            cancel_time += delta
+        else: 
+            cancel_time = wait_time
+    
 func on_ingredient_entered(ingredient: Node):
-	if ingredient is IngredientBlock:
-		ingredients.append(ingredient)
+    if ingredient is IngredientBlock:
+        ingredients.append(ingredient)
 
 func on_ingredient_exited(ingredient: Node):
-	if ingredient is IngredientBlock:
-		var idx = ingredients.find(ingredient)
-		if idx != -1:
-			ingredients.remove_at(idx)
+    if ingredient is IngredientBlock:
+        var idx = ingredients.find(ingredient)
+        if idx != -1:
+            ingredients.remove_at(idx)
 
 func recipe_is_correct(meal: Meal) -> bool:
-	if len(ingredients) != len(meal.ingredients):
-		return false
-	return true
+    if len(ingredients) != len(meal.ingredients):
+        return false
+    return true
 
 func create_meal():
-	if (cooking): return
+    if (cooking): cancel_meal()
 
-	if _pull_tween: _pull_tween.kill()
-	_pull_tween = create_tween()
-	_pull_tween.tween_property(pull_thingy, "position:y", -215.0, 0.45).set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_BOUNCE)
+    if _pull_tween: _pull_tween.kill()
+    _pull_tween = create_tween()
+    _pull_tween.tween_property(pull_thingy, "position:y", -215.0, 0.45).set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_BOUNCE)
 
-	print("Cooking")
-	cooking = true
-	timer.start()
-		
+    cooking = true
+    timer.start()
+
+    print("Cooking")
+    
+func cancel_meal():
+    cooking = false
+    cancel_time = timer.time_left
+    timer.stop()
+
+    print("Cancelled")
+        
 func finish_meal(): 
-	var orders = OrderBook.get_orders()
-	for order_id in orders:
-		var order = orders[order_id]
+    var orders = OrderBook.get_orders()
+    for order_id in orders:
+        var order = orders[order_id]
 
-		var meal_idx = order.needs_ingredients(ingredients)
-		if meal_idx != -1: 
-			order.complete_meal(meal_idx)
-			break
+        var meal_idx = order.needs_ingredients(ingredients)
+        if meal_idx != -1: 
+            order.complete_meal(meal_idx)
+            break
 
-	for i in ingredients:
-		i.queue_free()
+    for i in ingredients:
+        i.queue_free()
 
-	if _pull_tween: _pull_tween.kill()
-	_pull_tween = create_tween()
-	_pull_tween.tween_property(pull_thingy, "position:y", -315.0, 0.45).set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_BOUNCE)
+    if _pull_tween: _pull_tween.kill()
+    _pull_tween = create_tween()
+    _pull_tween.tween_property(pull_thingy, "position:y", -315.0, 0.45).set_ease(Tween.EASE_OUT).set_trans(Tween.TRANS_BOUNCE)
 
-	SignalBus.done_cooking.emit()
-	cooking = false
+    SignalBus.done_cooking.emit()
+    cooking = false
+    cancel_time = wait_time
---
+++ entities/pull_thingy.gd
@@ -0,0 +1,65 @@
+extends Sprite2D
+class_name PullThingy
+
+var _is_pressed_on_sprite = false
+var start_global_y: float
+var init_y: float
+var target_pos: float
+
+@export var max_pullout: float = 100.0 
+
+var is_pressed: bool 
+var threshold: float = 0.95
+
+signal cancelled
+signal pressed
+
+
+func _ready() -> void:
+    init_y = position.y
+    SignalBus.done_cooking.connect(release)
+
+func _unhandled_input(event):
+    if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
+        var is_inside = get_rect().has_point(get_local_mouse_position())
+        
+        if event.is_pressed():
+            if is_inside:
+                _is_pressed_on_sprite = true
+                start_global_y = get_global_mouse_position().y
+                get_viewport().set_input_as_handled()
+            else:
+                _is_pressed_on_sprite = false
+                
+        elif event.is_released():
+            if _is_pressed_on_sprite:
+                get_viewport().set_input_as_handled()
+            _is_pressed_on_sprite = false
+
+func _process(delta: float) -> void:
+    if not _is_pressed_on_sprite: 
+        target_pos = init_y
+        if is_pressed:
+            cancelled.emit()
+            is_pressed = false
+    else:
+        var current_global_y = get_global_mouse_position().y
+        var pull_distance = current_global_y - start_global_y
+
+        var pullout = clamp(pull_distance, 0.0, max_pullout)
+        var percentage = pull_distance / max_pullout
+        
+        if not is_pressed and percentage >= threshold:
+            is_pressed = true 
+            pressed.emit()
+        elif is_pressed and percentage <= threshold:
+            is_pressed = false
+            cancelled.emit()
+
+        target_pos = init_y + pullout
+    
+    position.y = lerp(position.y, target_pos, 10.0 * delta)
+    
+func release(): 
+    is_pressed = false 
+    _is_pressed_on_sprite = false
