bb3ad7731c4a685325727c61ee5b7610f5f1931c
Fabian Friedrich <friedrich.fabian@gmx.net>
2026-09-23 18:18:31 +0200

    add: auto dropping of ingredients

--- scenes/main_menu.gd
+++ scenes/main_menu.gd
@@ -4,24 +4,24 @@ extends Control
 @onready var clouds: Sprite2D = $Clouds
 
 func _ready() -> void:
-	$"HBoxContainer/Start Button".should_oscillate = true
-	$"HBoxContainer/Credits Button".should_oscillate = true
-	$"HBoxContainer/Quit Button".should_oscillate = true
-	credits.hide()
+    $"HBoxContainer/Start Button".should_oscillate = true
+    $"HBoxContainer/Credits Button".should_oscillate = true
+    $"HBoxContainer/Quit Button".should_oscillate = true
+    credits.hide()
 
 func _process(delta: float) -> void:
-	if clouds.global_position.x < 2690: clouds.global_position.x += delta * 10
-	elif clouds.global_position.x >= 2690:
-		clouds.global_position.x = -1280
-		clouds.global_position.y = randi_range(568, 768)
+    if clouds.global_position.x < 2690: clouds.global_position.x += delta * 10
+    elif clouds.global_position.x >= 2690:
+        clouds.global_position.x = -1280
+        clouds.global_position.y = randi_range(568, 768)
 
 func _on_start_button_button_up() -> void:
-	GameManager.reset()
-	SignalBus.game_state_change.emit("gameplay")
-	ScreenFader.change_scene("res://scenes/game.tscn")
+    GameManager.reset()
+    SignalBus.game_state_change.emit("gameplay")
+    ScreenFader.change_scene("res://scenes/game.tscn")
 
 func _on_credits_button_button_up() -> void:
-	credits.show()
+    credits.show()
 
 func _on_quit_button_button_up() -> void:
-	ScreenFader.change_scene("QUIT")
+    ScreenFader.change_scene("QUIT")
